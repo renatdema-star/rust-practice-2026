e42b64c8f638be1deb64b06a5f9d0300641e4f29
Renat Dema <renatdema@gmail.com>
2026-03-11 01:45:31 +0200

    Практична 7: Between Two Sets завершено

--- src/hackerrank/mod.rs
+++ src/hackerrank/mod.rs
@@ -3,7 +3,7 @@ mod task03;
 pub mod task04;
 pub mod task05;
 pub mod task06;
-mod task07;
+pub mod task07;
 mod task08;
 mod task09;
 mod task10;
--- src/hackerrank/task07.rs
+++ src/hackerrank/task07.rs
@@ -1,1 +1,36 @@
 // https://www.hackerrank.com/challenges/between-two-sets/problem
+#![allow(dead_code)]
+
+pub fn get_total_x(a: &[i32], b: &[i32]) -> i32 {
+    // Знаходимо діапазон для перевірки: від найбільшого в a до найменшого в b
+    let max_a = *a.iter().max().unwrap_or(&1);
+    let min_b = *b.iter().min().unwrap_or(&100);
+
+    let mut count = 0;
+
+    // Перебираємо всі числа в цьому діапазоні
+    for x in max_a..=min_b {
+        // Перевіряємо обидві умови через ітератори
+        let is_multiple_of_a = a.iter().all(|&num| x % num == 0);
+        let is_factor_of_b = b.iter().all(|&num| num % x == 0);
+
+        if is_multiple_of_a && is_factor_of_b {
+            count += 1;
+        }
+    }
+
+    count
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_between_two_sets() {
+        let a = vec![2, 4];
+        let b = vec![16, 32, 96];
+        // Очікуємо 3 числа: 4, 8, 16
+        assert_eq!(get_total_x(&a, &b), 3);
+    }
+}
